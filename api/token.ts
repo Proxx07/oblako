0c99c9c7824aa7a23b2b0730fe01f5c68a9b06e8
Proxx07 <zakir.pulatov97@gmail.com>
2025-10-04 06:54:21 +0500

    [DONE] vercel proxy

--- api/token.ts
+++ api/token.ts
@@ -1,6 +1,26 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import tokenHandler from '../server/index';
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  return tokenHandler(req, res);
+export default async (req: VercelRequest, res: VercelResponse) => {
+  try {
+    // eslint-disable-next-line node/prefer-global/process
+    const response = await fetch(`${process.env.API_URL}/api/1/access_token`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // eslint-disable-next-line node/prefer-global/process
+      body: JSON.stringify({ apiLogin: process.env.API_LOGIN }),
+    });
+
+    const result = await response.json();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Set-Cookie', [`token=${result.token}; Path=/; SameSite=Strict; Secure`]);
+
+    (res as any).statusCode = 200;
+    res.end(JSON.stringify({ success: true }));
+  }
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  catch (e) {
+    (res as any).statusCode = 500;
+    res.end(JSON.stringify({ success: false }));
+  }
 };
